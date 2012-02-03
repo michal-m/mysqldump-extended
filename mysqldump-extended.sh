ac530101c65bbdf9459104f19b08f47d4fd12cd7
MichalM <michal.j.musial@gmail.com>
2012-02-03 22:21:13 +0100

    - changed -t|--skip-tarballing to -z|--tar-gz and SKIP_TARBALLING to TAR_GZ - added -b|--database parameter, if specified privileges are not dumped - cleanup in verbose calls - stat is only called if found and executable
    closes #1
    closes #4

--- mysqldump-extended.sh
+++ mysqldump-extended.sh
@@ -18,6 +18,7 @@ function verbose {
 
 # Initial Variable definitions
 DATE=`date +'%Y%m%d'`
+TAR="/usr/bin/tar"
 # MYSQL_USER must have following global privileges:
 # SELECT, SHOW DATABASES, LOCK TABLES, EVENT, TRIGGER, SHOW VIEW
 MYSQL_USER="mysqldump"
@@ -31,6 +32,11 @@ DUMPS_DIRNAME="mysqldumps_${DATE}"
 while :
 do
     case "$1" in
+        -b | --database)
+            if [ -z "$2" ]; then echo "Error: Database name not specified" >&2; exit 1; fi
+            DATABASE_NAME=$2
+            shift 2
+            ;;
         -c | --default-charset)
             if [ -z "$2" ]; then echo "Error: Default character set not specified" >&2; exit 1; fi
             MYSQL_CHARSET=$2
@@ -69,8 +75,8 @@ do
             VERBOSE="verbose"
             shift
             ;;
-        -t | --skip-tarballing)
-            SKIP_TARBALLING="skip tarballing"
+        -z | --tar-gz)
+            TAR_GZ="tar gz"
             shift
             ;;
 #        --) # End of all options
@@ -92,16 +98,16 @@ if [ ! -w "$OUTPUT_DIR" ]; then echo "Error: Output directory is not writable" >
 if [ -e "${OUTPUT_DIR}/${OUTPUT_FILE}" ]; then echo "Error: Specified output file already exists" >&2; exit 1; fi
 if [ -z "$MYSQL_PASSWORD" ]; then echo "Error: MySQL password not provided or empty" >&2; exit 1; fi
 if [ -e "${OUTPUT_DIR}/${DUMPS_DIRNAME}" ]; then echo "Error: Output directory already contains a file/folder with the same name as temporary folder required: ${OUTPUT_DIR}/$DUMPS_DIRNAME" >&2; exit 1; fi
+if [ "$TAR_GZ" ] && [ ! -x "$TAR" ]; then echo "Error: Tar not found or not executable (looking at: $TAR)" >&2; exit 1; fi
 
 # OK, let's roll
-verbose "START\n" 1
+verbose "START\n"
 
 STATIC_PARAMS="--default-character-set=$MYSQL_CHARSET --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD"
 MYSQLDUMP="/usr/local/bin/mysqldump"
 MYSQL="/usr/local/bin/mysql"
 
 STAT="/usr/bin/stat"
-TAR="/usr/bin/tar"
 
 if [ "$SKIP_DELETE_PREVIOUS" ]; then
     verbose "NOT deleting any old backups..."
@@ -113,16 +119,21 @@ fi
 verbose "\nCreating temporary folder: ${DUMPS_DIRNAME}."
 mkdir ${OUTPUT_DIR}/${DUMPS_DIRNAME}
 
-verbose "\nRetrieving list of all databases... " 1
-aDatabases=( $($MYSQL $STATIC_PARAMS -N -e "SHOW DATABASES;" | grep -Ev "(test|information_schema|mysql|performance_schema|phpmyadmin)") )
-verbose "done."
-verbose "Found" ${#aDatabases[@]}" valid database(s).\n"
+if [ "$DATABASE_NAME" ]; then
+    verbose "\nDatabase to be dumped: $DATABASE_NAME"
+    sDatabases=$DATABASE_NAME
+else
+    verbose "\nRetrieving list of all databases...\t" 1
+    aDatabases=( $($MYSQL $STATIC_PARAMS -N -e "SHOW DATABASES;" | grep -Ev "(test|information_schema|mysql|performance_schema|phpmyadmin)") )
+    verbose "done."
+    verbose "Found ${#aDatabases[@]} valid database(s).\n"
 
-sDatabases=${aDatabases[*]}
+    sDatabases=${aDatabases[*]}
+fi
 
 verbose "Beginning dump process..."
 for db in $sDatabases; do
-    verbose "- dumping '${db}'... " 1
+    verbose "- dumping '${db}'...\t" 1
     SECONDS=0
     # dumping database tables structure
     $MYSQLDUMP $STATIC_PARAMS \
@@ -171,35 +182,41 @@ for db in $sDatabases; do
         --skip-triggers \
         --databases $db > ${OUTPUT_DIR}/${DUMPS_DIRNAME}/$db.5-ROUTINES.sql
 
-    verbose "done in" $SECONDS "second(s);"
+    verbose "done in $SECONDS second(s);"
 done
 
-verbose "- dumping PRIVILEGES... " 1
-SECONDS=0
-$MYSQL $STATIC_PARAMS -B -N -e "SELECT DISTINCT CONCAT(
-        'SHOW GRANTS FOR ''', user, '''@''', host, ''';'
-        ) AS query FROM mysql.user" | \
-        $MYSQL $STATIC_PARAMS | \
-        sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "${OUTPUT_DIR}/${DUMPS_DIRNAME}/PRIVILEGES.sql"
-verbose "done in" $SECONDS "second(s)."
+if [ -z "$DATABASE_NAME" ]; then
+    verbose "- dumping PRIVILEGES...\t" 1
+    SECONDS=0
+    $MYSQL $STATIC_PARAMS -B -N -e "SELECT DISTINCT CONCAT(
+            'SHOW GRANTS FOR ''', user, '''@''', host, ''';'
+            ) AS query FROM mysql.user" | \
+            $MYSQL $STATIC_PARAMS | \
+            sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "${OUTPUT_DIR}/${DUMPS_DIRNAME}/PRIVILEGES.sql"
+    verbose "done in  $SECONDS second(s)."
+fi
+
 verbose "Dump process completed."
 
-if [ "$SKIP_TARBALLING" ]; then
-    verbose "\nSkipping tarballing sql dumps"
-else
-    verbose "\nTarballing all sql dumps... " 1
+if [ "$TAR_GZ" ]; then
+    verbose "\nTarballing all sql dumps...\t" 1
     cd ${OUTPUT_DIR}
     SECONDS=0
     $TAR cfz ${OUTPUT_FILE} ${DUMPS_DIRNAME}
-    verbose "done in" $SECONDS "second(s)."
+    verbose "done in  $SECONDS second(s)."
 
-    output_file_size=`$STAT -f %z $OUTPUT_FILE`
-
-    verbose "\nDeleting sql files... " 1
+    verbose "\nDeleting sql files...\t" 1
     rm -fvR ${OUTPUT_DIR}/${DUMPS_DIRNAME}
     verbose "done."
 
-    verbose "\nFinal dump file: ${OUTPUT_DIR}/${OUTPUT_FILE} (${output_file_size} bytes).\n"
+    verbose "\nFinal dump file: ${OUTPUT_DIR}/${OUTPUT_FILE}" 1
+
+    if [ -x "$STAT" ]; then
+        output_file_size=`$STAT -f %z $OUTPUT_FILE`
+        verbose " (${output_file_size} bytes).\n"
+    else
+        verbose "\n"
+    fi
 fi
 
 verbose "END."

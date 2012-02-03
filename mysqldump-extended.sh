534976a40590d03daa8c73926586097145b82773
MichalM <michal.j.musial@gmail.com>
2012-02-03 20:12:21 +0100

    script now accepts -v|--verbose parameter and all echos are now handled by an internal verbose function
    closes #8

--- mysqldump-extended.sh
+++ mysqldump-extended.sh
@@ -5,8 +5,39 @@
 # http://soniahamilton.wordpress.com/2005/11/16/backup-multiple-databases-into-separate-files/
 # http://mysqlpreacher.com/wordpress/2010/08/dumping-ddl-mysqldump-tables-stored-procedures-events-triggers-separately/
 
-echo "START"
-echo
+# Functions
+function verbose {
+    if [ "$VERBOSE" ]; then
+        if [ "$2" ]; then
+            echo -en $1;
+        else
+            echo -e $1;
+        fi
+    fi
+}
+
+# Parse commandline options first
+while :
+do
+    case "$1" in
+        -v | --verbose)
+            VERBOSE="verbose"
+            shift
+            ;;
+#        --) # End of all options
+#            shift
+#            break;
+        -*)
+            echo "Error: Unknown option: $1" >&2
+            exit 1
+            ;;
+        *)  # No more options
+            break
+            ;;
+    esac
+done
+
+verbose "START\n" 1
 
 DATE=`date +'%Y%m%d'`
 
@@ -24,26 +55,23 @@ STAT="/usr/bin/stat"
 TAR="/usr/bin/tar"
 OUTPUT_FILE="mysqldumps.tar.gz"
 
-echo "Deleting any old backups..."
+verbose "Deleting any old backups..."
 rm -fv ${DIR_BACKUP}/mysqldump*.tar.gz
 
-echo
-echo "Creating temporary folder: ${DIR_SQL}."
+verbose "\nCreating temporary folder: ${DIR_SQL}."
 mkdir ${DIR_BACKUP}/${DIR_SQL}
 
-echo
-echo -n "Retrieving list of all databases... "
+verbose "\nRetrieving list of all databases... " 1
 aDatabases=( $($MYSQL --user=$MYSQL_USER --password=$MYSQL_PASSWORD -N -e "SHOW DATABASES;" | grep -Ev "(test|information_schema|mysql|performance_schema|phpmyadmin)") )
-echo "done."
-echo "Found" ${#aDatabases[@]}" valid database(s)."
-echo
+verbose "done."
+verbose "Found" ${#aDatabases[@]}" valid database(s).\n"
 
 sDatabases=${aDatabases[*]}
 
-echo "Beginning dump process..."
+verbose "Beginning dump process..."
 STATIC_PARAMS="--default-character-set=utf8 --user=$MYSQL_USER --password=$MYSQL_PASSWORD"
 for db in $sDatabases; do
-    echo -n "- dumping '${db}'... "
+    verbose "- dumping '${db}'... " 1
     SECONDS=0
     # dumping database tables structure
     $MYSQLDUMP \
@@ -97,35 +125,30 @@ for db in $sDatabases; do
         $STATIC_PARAMS \
         --databases $db > ${DIR_BACKUP}/${DIR_SQL}/$db.5-ROUTINES.sql
 
-    echo "done in" $SECONDS "second(s);"
+    verbose "done in" $SECONDS "second(s);"
 done
 
-echo -n "- dumping PRIVILEGES... "
+verbose "- dumping PRIVILEGES... " 1
 SECONDS=0
 $MYSQL -B -N --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "SELECT DISTINCT CONCAT(
         'SHOW GRANTS FOR ''', user, '''@''', host, ''';'
         ) AS query FROM mysql.user" | \
         $MYSQL --user=$MYSQL_USER --password=$MYSQL_PASSWORD | \
         sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' > "${DIR_BACKUP}/${DIR_SQL}/PRIVILEGES.sql"
-echo "done in" $SECONDS "second(s)."
-echo "Dump process completed."
+verbose "done in" $SECONDS "second(s)."
+verbose "Dump process completed."
 
-echo
-echo -n "Tarballing all sql dumps... "
+verbose "\nTarballing all sql dumps... " 1
 cd ${DIR_BACKUP}
 SECONDS=0
 $TAR cfz ${OUTPUT_FILE} ${DIR_SQL}
-echo "done in" $SECONDS "second(s)."
+verbose "done in" $SECONDS "second(s)."
 
 output_file_size=`$STAT -f %z $OUTPUT_FILE`
 
-echo
-echo "Deleting sql files... "
+verbose "\nDeleting sql files... " 1
 rm -fvR ${DIR_BACKUP}/${DIR_SQL}
-echo "done."
+verbose "done."
 
-echo
-echo "Final dump file: ${DIR_BACKUP}/${OUTPUT_FILE} (${output_file_size} bytes)."
-echo
-
-echo "END."
+verbose "\nFinal dump file: ${DIR_BACKUP}/${OUTPUT_FILE} (${output_file_size} bytes).\n"
+verbose "END."
